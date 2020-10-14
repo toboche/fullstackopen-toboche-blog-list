18f8024a5f32d6537866b5d4978c30d1792727a1
toboche <toboche@gmail.com>
2020-10-14 19:36:24 +0200

    4.4

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -6,3 +6,40 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+    test('0 for 0', () => {
+        const testList = []
+        expect(listHelper.totalLikes(testList)).toBe(0)
+    })
+
+    test('1 for 1', () => {
+        const testList = [
+            {
+                    title: "test title",
+                    author: "noone",
+                    url: "www.google.com",
+                    likes: 1
+            }
+        ]
+        expect(listHelper.totalLikes(testList)).toBe(1)
+    })
+
+    test('123 for 122 + 1', () => {
+        const testList = [
+            {
+                    title: "test title",
+                    author: "noone",
+                    url: "www.google.com",
+                    likes: 122
+            },
+            {
+                title: "test title",
+                author: "noone",
+                url: "www.google.com",
+                likes: 1
+        }
+        ]
+        expect(listHelper.totalLikes(testList)).toBe(123)
+    })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
     return 1
   }
   
+const totalLikes = (blogs) => {
+    return blogs.map(it => it.likes)
+        .reduce ((acc, value) => acc + value, 0)
+}
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
